f6dbe740a3e0c6efc914e5d7edf3034e1ccd62ba
bennacer860 <bennacer860@gmail.com>
2017-03-31 21:39:13 -0400

    implementing cache without an expiration date

--- main.rb
+++ main.rb
@@ -9,12 +9,32 @@ class Main < Sinatra::Base
 
   def number_of_users
     Cache.fetch "number_of_users" do
-      FakeDatabase.connection "select count(*) from users"
+      FakeDatabaseAdapter.connection "select count(*) from users"
     end
   end
 end
 
-class FakeDatabase
+class Cache
+  @memory = {}
+  def self.fetch key, &block
+    if @memory.key?(key)
+      # fetch and return result
+      puts "fetch from cache"
+      @memory[key]
+    else
+      if block_given?
+        # make the DB query and create a new entry for the request result
+        puts "did not find key in cache, executing block ..."
+        @memory[key] = yield(block)
+      else
+        # no block given, do nothing
+        nil
+      end
+    end
+  end
+end
+
+class FakeDatabaseAdapter
   def self.connection query
     puts "executing : #{query}"
     return {count: 33}
